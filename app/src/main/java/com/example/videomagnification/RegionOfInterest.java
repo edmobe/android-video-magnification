b9747c3824dfff5d8eed7bcec62dd746d52376d2
edmobe <e.d.mobe@hotmail.com>
2021-09-24 11:45:52 -0600

    Add ROI activity

---
+++ app/src/main/java/com/example/videomagnification/RegionOfInterest.java
@@ -0,0 +1,14 @@
+package com.example.videomagnification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegionOfInterest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_region_of_interest);
+    }
+}
